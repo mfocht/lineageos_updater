3d80b600f2407518a07410b0b81d7f3bc910cbec
Micah Focht <bandmfocht@gmail.com>
2017-01-21 16:14:29 -0500

    Update seed.sh
    Fixed a missing r.

--- seed.sh
+++ seed.sh
@@ -11,7 +11,7 @@ VERSION=14.1
 DATETIME="2017-07-07"
 ROMTYPE="nightly"
 MD5SUM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-URL=https://mirrobits.lineageos.org/full/${DEVICE}/${DATETIME}/${FILENAME}
+URL=https://mirrorbits.lineageos.org/full/${DEVICE}/${DATETIME}/${FILENAME}
 
 
 flask addrom --filename $FILENAME --device $DEVICE --version $VERSION --datetime $DATETIME --romtype $ROMTYPE --md5sum $MD5SUM --url $URL --available true
